90c639e64337b629975218ea792cbd0d4ab392d4
Ryan <547558@student.fontys.nl>
2025-09-16 16:36:09 +0200

    Busy with animations (idle done but not implemented yet)

--- Scripts/playerController.gd
+++ Scripts/playerController.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
+@export var speed = 10.0
+@export var jumpPower = 10.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var speedMultiplier = 30.0
+var jumpMultiplier = -30.0
+var direction = 0
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
@@ -12,14 +17,14 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jumpPower * jumpMultiplier
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("moveLeft", "MoveRight")
+	direction = Input.get_axis("moveLeft", "MoveRight")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed * speedMultiplier
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed * speedMultiplier)
 
 	move_and_slide()
